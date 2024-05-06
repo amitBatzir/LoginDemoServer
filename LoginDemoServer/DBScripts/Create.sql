d4846ca4eeccbb9a918ebf4790bfaa268c672b6d
User <User@LAB2-5>
2024-05-06 14:10:17 +0300

    Started the Ex

--- LoginDemoServer/DBScripts/Create.sql
+++ LoginDemoServer/DBScripts/Create.sql
@@ -11,8 +11,21 @@ Create Table Users (
 	[Name] nvarchar(50) NOT NULL
 )
 Go
+Create Table Grades(
+Id int IDENTITY (100,1) PRIMARY KEY,
+DateOfTest DateTime NULL,
+[Subject] nvarchar(20) NOT NULL,
+Grade int NOT NULL,
+Email nvarchar(50) NOT NULL FOREIGN KEY REFERENCES Users(Email) 
+)
+Go
+
 
 INSERT INTO dbo.Users VALUES ('ofer@ofer.com', '1234', '+972526344450','15-nov-1972',1,'Ofer Zadikario')
-Go
+INSERT INTO dbo.Grades VALUES ( '15-NOV-2000', 'English',100,'ofer@ofer.com')
+Go  
+
+select * from dbo.Users
+select * from dbo.Grades
 
 --scaffold-DbContext "Server = (localdb)\MSSQLLocalDB;Initial Catalog=LoginDemoDB;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Encrypt=False;Trust Server Certificate=False;Command Timeout=0" Microsoft.EntityFrameworkCore.SqlServer -OutPutDir Models -Context LoginDemoDbContext -DataAnnotations -force
